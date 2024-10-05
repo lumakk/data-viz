bdb9c7c0a9ea5ec0901cbb6e72157de931641a68
lumakk <macedisonlozano@gmail.com>
2024-10-05 14:29:43 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+// Current Date for Title
+const dateElement = document.getElementById('date');
+
+console.log(dateElement);
+
+let currentDate = new Date();
+
+console.log(currentDate);
+
+// "dateOptions" Object will allow us to change the format retrieve from currentDate.
+let dateOptions
