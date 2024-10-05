5b7a98837d2633301586c086766c0b0934f0efbf
lumakk <macedisonlozano@gmail.com>
2024-10-05 15:23:16 +0800

    js scri[t

--- index.js
+++ index.js
@@ -10,4 +10,74 @@ let currentDate = new Date();
 console.log(currentDate);
 
 // "dateOptions" Object will allow us to change the format retrieve from currentDate.
-let dateOptions
+let dateOptions = {year: 'numeric', month: 'long', day: "numeric"};
+
+// Will allow us to change the format to US time using the "dateOptions" object
+dateElement.innerHTML = currentDate.toLocaleDateString('en-US', dateOptions);
+
+// Rapid API code to retrieve trending twitter topics
+const url = 'https://twitter-trends5.p.rapidapi.com/twitter/request.php';
+const data = new FormData();
+data.append('woeid', '23424934');
+
+const options = {
+	method: 'POST',
+	headers: {
+		'x-rapidapi-key': 'a3352c04bemsh64eaa1c90227388p1eaaa1jsn3b27de6d9161',
+		'x-rapidapi-host': 'twitter-trends5.p.rapidapi.com'
+	},
+	body: data
+};
+
+// Dummy data
+let myPost = {
+    name:"#AlwaysToYouHannie",
+    query:"%23AlwaysToYouHannie",
+    url:"search?q=%23AlwaysToYouHannie",
+    volume:98800,
+    volumeShort:"97.9K",
+    domainContext:"Korean music",
+    groupedTrends:[]
+}
+
+console.log(myPost);
+// dot notation - to access a property or method of an object
+console.log(myPost.name);
+console.log(myPost.url);
+console.log(myPost.volume);
+
+// Array of objects
+let graphData = [
+    {
+        name:"2ne1",
+        query:"2ne1",
+        url:"search?q=2ne1",
+        volume:394000,
+        volumeShort:"394K",
+        domainContext:"K-pop",
+        groupedTrends:[]
+    },
+    {
+        name:"#FudgeeBarrxBINI",
+        query:"%23FudgeeBarrxBINI",
+        url:"search?q=%23FudgeeBarrxBINI",
+        volume:23200,
+        volumeShort:"22.6K",
+        domainContext:"",
+        groupedTrends:[]
+
+    }
+];
+
+console.log(graphData);
+// Array uses "index" to select  the position of an element.
+console.log(graphData[0]);
+
+// using dot notation
+console.log(graphData[0].name);
+
+// fetch(url, options)
+// .then(res => res.json())
+// .then(data =>{
+//     console.log(data);
+// });
